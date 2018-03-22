10e3b6676320cf290e5b77be58669e9a76e892de
matthewcarlreetz <matthewcarlreetz@gmail.com>
2018-03-21 22:05:41 -0500

    Added mysql

--- config/connections.js
+++ config/connections.js
@@ -42,10 +42,11 @@ module.exports.connections = {
   ***************************************************************************/
   // someMysqlServer: {
   //   adapter: 'sails-mysql',
-  //   host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-  //   user: 'YOUR_MYSQL_USER', //optional
-  //   password: 'YOUR_MYSQL_PASSWORD', //optional
-  //   database: 'YOUR_MYSQL_DB' //optional
+  //   host: 'localhost',
+  //   user: 'root',
+  //   port: 3306,
+  //   password: '',
+  //   database: ''
   // },
 
   /***************************************************************************
